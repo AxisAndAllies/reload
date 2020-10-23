f5861d71c98af125d212b8407d1e7b7548316b8a
cktang88 <arctangent20@yahoo.com>
2020-10-23 18:45:43 -0500

    add chakra

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ yarn create next-app --example with-typescript .
 ```
 
 also uses ChakraUI + react-hotkeys-hook
+
+- hold down "r" to reload, press "r" to close bolt/slide action at end of reload
+- tactical reload (may or may not be empty) - pick new mag, swap mag, keep old mag, 3.5s
+  - tap r, hold r 1/2 way, cmd+r 1/2 way, tap r if empty
+- speed reload (usually mag empty) - pick new mag, old mag drops to ground, new mag in, slide action, 2.5s
+  - tap r, hold r, tap r
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,119 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
+import {
+  Box,
+  Button,
+  Text,
+  Progress,
+  Flex,
+  Tag,
+  useColorMode,
+} from "@chakra-ui/core";
+import { useHotkeys } from "react-hotkeys-hook";
+import { useState, useEffect } from "react";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+// import create from "zustand";
 
-export default IndexPage
+// const useStore = create((set) => ({
+//   bears: 0,
+//   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+//   removeAllBears: () => set({ bears: 0 }),
+// }));
+
+const downOnly = {
+  keydown: true,
+  keyup: false,
+};
+const upOnly = {
+  keydown: false,
+  keyup: true,
+};
+const preventDefaultWrap = (fn: () => void) => (event: KeyboardEvent) => {
+  fn();
+  event.preventDefault();
+};
+
+const IndexPage = () => {
+  // <Layout title="Home | Next.js + TypeScript Example">
+  //   <h1>Hello Next.js 👋</h1>
+  //   <p>
+  //     <Link href="/about">
+  //       <a>About</a>
+  //     </Link>
+  //   </p>
+  // </Layout>
+  const MAG_MAX = 9;
+  const [count, setCount] = useState(0);
+  const [inMag, setInMag] = useState(MAG_MAX);
+  const [mags, setMags] = useState([MAG_MAX, MAG_MAX, MAG_MAX, MAG_MAX]);
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    if (count >= 100) {
+      setCount(0);
+      setInMag(MAG_MAX);
+      console.log("reloaded");
+    }
+    console.log(count);
+  }, [count]);
+
+  useEffect(() => {
+    if (inMag === 0) {
+      console.log("mag empty");
+    }
+  }, [inMag]);
+
+  const addCount = (add: number) => {
+    setCount((prev) => prev + add);
+  };
+
+  useHotkeys(
+    "r",
+    preventDefaultWrap(() => addCount(10)),
+    upOnly
+  );
+  useHotkeys(
+    "ctrl+r",
+    preventDefaultWrap(() => addCount(1)),
+    downOnly
+  );
+
+  useHotkeys(
+    "ctrl+e",
+    preventDefaultWrap(() => setCount(0)),
+    downOnly
+  );
+  useHotkeys(
+    "space",
+    preventDefaultWrap(() => setInMag((prev) => Math.max(prev - 1, 0))),
+    downOnly
+  );
+  return (
+    <Box m={10}>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+      <Text fontSize={30}>{inMag}</Text>
+      <Button colorScheme="teal" variant="outline" m={2}>
+        asdf
+      </Button>
+      <Button colorScheme="orange" variant="outline" m={2}>
+        eeee
+      </Button>
+      <Box maxWidth={300}>
+        <Progress colorScheme="green" height="32px" value={count} />
+      </Box>
+      <Flex>
+        {mags.map((mag) => (
+          <Box m={2}>
+            <Tag fontSize={20}>{mag}</Tag>
+          </Box>
+        ))}
+      </Flex>
+      <Text></Text>
+    </Box>
+  );
+};
+
+export default IndexPage;
